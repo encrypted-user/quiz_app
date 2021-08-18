b79251e566676ce26eecb530e775a20a2783c8a7
Prabhat <prabhatjain15.2001@gmail.com>
2021-08-19 00:18:08 +0530

    Add Ternary Operator

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favorite color?',
         'answers': ['Black', 'Red', 'Green', 'White']
@@ -41,18 +41,24 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map(
-                  (answer) => Answer(answer, _answerQuestion),
-                )
-                .toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map(
+                        (answer) => Answer(answer, _answerQuestion),
+                      )
+                      .toList()
+                ],
+              )
+            : Center(
+                child: Text(
+                  'You did it !',
+                ),
+              ),
       ),
     );
   }
